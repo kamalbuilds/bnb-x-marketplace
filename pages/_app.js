4241b60add7f4501821ff74e3610d5d57f6fa947
Kamal Nayan <geniusamansingh@gmail.com>
2023-04-26 19:29:04 +0000

    bug fixing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import dynamic from 'next/dynamic';
 import DynamicWagmi from '../src/components/dynamicWagmi';
+import 'antd-mobile/es/components/action-sheet/action-sheet';
 
 // const DynamicWagmi = dynamic(() => import('../src/components/dynamicWagmi/index'), {
 //     ssr: false,
