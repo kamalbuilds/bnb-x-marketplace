42e1a38be5e2885a2290b6f42824660bca9218ed
Kamal Nayan <geniusamansingh@gmail.com>
2023-05-12 13:19:25 +0000

    code cleanup

--- Chat/src/index.tsx
+++ Chat/src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 // import App from './App_testAuthToDapp';
 // import App from './App_testBindDid';
 // import App from './App_testFollowUser';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,8 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
